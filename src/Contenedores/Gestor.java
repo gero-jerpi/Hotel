0c7a6268516f3c47cbab85be2bf4066f2338c059
gero-jerpi <gero_jerpi@hotmail.com>
2024-11-16 18:43:29 -0300

    CAMBIOS
    agrego estructura del proyecto y logica del gestor

---
+++ src/Contenedores/Gestor.java
@@ -0,0 +1,56 @@
+package Contenedores;
+
+import java.util.ArrayList;
+
+public class Gestor <T>{
+
+    /// ATRIBUTOS
+    private ArrayList<T> lista;
+
+    /// CONSTRUCTOR
+    public Gestor(){
+        this.lista = new ArrayList<>();
+    }
+
+
+    /// METODOS
+    public boolean agregar(T objeto){
+        if(objeto == null){
+            throw new IllegalArgumentException("NULO");
+        }
+
+        if(lista.contains(objeto)){
+            throw new IllegalArgumentException("CONTIENE");
+        }
+
+        lista.add(objeto);
+
+        return true;
+    }
+
+
+    /// GETTERS && SETTERS
+
+    public ArrayList<T> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<T> lista) {
+        this.lista = lista;
+    }
+
+
+    /// SOBREESCRITURA
+
+    @Override
+    public String toString() {
+        return "Gestor{" +
+                "lista=" + lista +
+                '}';
+    }
+
+
+
+
+
+}
---
+++ src/UI/Login.java
@@ -0,0 +1,20 @@
+package UI;
+
+public class Login {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
