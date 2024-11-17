23b1eeef16d2286a5dbd62eabd62f69307b8e03d
hewwwwwwwww <162480135+hewwwwwwwww@users.noreply.github.com>
2024-11-17 06:04:58 -0300

    Update Hotel.java / Interface Listar
    Agregado de métodos para listar dentro de la clase hotel, creación de la interfaz "Listar"

--- src/Modelo/Hotel.java
+++ src/Modelo/Hotel.java
@@ -3,8 +3,10 @@ package Modelo;
 import Contenedores.Gestor;
 import ENUMS.Estado;
 
+import java.util.Iterator;
 
-public class Hotel{
+
+public class Hotel {
 
     /// ATRIBUTOS
     private int id;
@@ -16,7 +18,7 @@ public class Hotel{
     private static int incremento = 0;
 
     /// CONSTRUCTORES
-    public Hotel(String nombre){
+    public Hotel(String nombre) {
         this.id = incremento++;
         this.nombre = nombre;
         this.habitaciones = new Gestor<>();
@@ -24,7 +26,7 @@ public class Hotel{
         this.usuarios = new Gestor<>();
     }
 
-    public Hotel(){
+    public Hotel() {
         this.id = incremento++;
         this.habitaciones = new Gestor<>();
         this.reservas = new Gestor<>();
@@ -69,20 +71,100 @@ public class Hotel{
     }
 
     /// METODOS
-    public boolean agregar(Object objeto){
-        if(objeto instanceof Habitacion){
+    public boolean agregar(Object objeto) {
+        if (objeto instanceof Habitacion) {
             habitaciones.agregar((Habitacion) objeto);
-        }else if(objeto instanceof Reserva){
+        } else if (objeto instanceof Reserva) {
             reservas.agregar((Reserva) objeto);
             ((Habitacion) objeto).setEstado(Estado.OCUPADA); ///VERIFICAR LUEGO
-        }else if(objeto instanceof Usuario){
+        } else if (objeto instanceof Usuario) {
             usuarios.agregar((Usuario) objeto);
-        }else{
+        } else {
             throw new IllegalArgumentException("NO EXISTE");
         }
 
         return true;
     }
+
+
+    //metodo  de la clase recepcionista : listar ocupadas con datos de ocupantes , habitaciones disponibles, y habitaciones NO disponibles por algún motivo
+
+
+    //LISTA LAS HABITACIONES OCUPADAS CON LOS DATOS DE LOS OCUPANTES ^^
+
+    public String listarHabitacionesOcupadas() {
+        StringBuilder mensaje = new StringBuilder();
+
+        mensaje.append("Habitaciones ocupadas: \n");
+        Iterator<Reserva> iteradorReserva = reservas.getLista().iterator();
+
+
+        while (iteradorReserva.hasNext()) {
+            Reserva reserva = iteradorReserva.next();
+            if (reserva.getHabitacion().getEstado() == Estado.OCUPADA) {
+                mensaje.append("Habitación NRO ").append(reserva.getHabitacion().getNumeroHabitacion()).append("\n").append(reserva.getHabitacion().toString()).append("\n").append("Datos del cliente :\n").append(reserva.getCliente().toString());
+
+            }
+
+        }
+
+
+        return mensaje.toString();
+    }
+
+    //LISTA LAS HABITACIONES DISPONIBLES
+    public String listarHabitacionesDisponibles() {
+        StringBuilder mensaje = new StringBuilder();
+
+        Iterator<Habitacion> iterator = habitaciones.getLista().iterator();
+
+        mensaje.append("Habitaciones Disponibles : \n");
+        while ((iterator.hasNext())) {
+            Habitacion habitacion = iterator.next();
+
+            if (habitacion.getEstado() == Estado.DISPONIBLE) {
+                mensaje.append("Habitacion NRO ").append(habitacion.getNumeroHabitacion()).append("\n").append(habitacion.toString());
+
+
+            }
+
+        }
+
+        return mensaje.toString();
+    }
+
+    //LISTA LAS HABITACIONES NO DISPONIBLES Y SU ESTADO (EL POR QUÉ DE SU INDISPONIBILIDAD - OCUPADA - MANTENIMIENTO, ETC)
+    public String listarHabitacionesNOdisponibles() {
+        StringBuilder mensaje = new StringBuilder();
+        mensaje.append("Habitaciones NO disponibles : \n");
+        Iterator<Habitacion> iterator = habitaciones.getLista().iterator();
+        while (iterator.hasNext()) {
+            Habitacion habitacion = iterator.next();
+
+            if (habitacion.getEstado() != Estado.DISPONIBLE) {
+                mensaje.append("Habitacion NRO ").append(habitacion.getNumeroHabitacion()).append("\n").append(habitacion.toString()).append("\nMotivo : ").append(habitacion.getEstado());
+            }
+        }
+        return mensaje.toString();
+    }
+
+    //LISTA LAS RESERVAS DE UN CLIENTE
+
+    public String listarReservasXCliente(int id) {
+        StringBuilder mensaje = new StringBuilder();
+        Iterator<Reserva> iterador = reservas.getLista().iterator();
+        mensaje.append("Reservas del cliente :");
+        while (iterador.hasNext()) {
+            Reserva reserva = iterador.next();
+
+            if (reserva.getCliente().getId() == id) {
+                mensaje.append(reserva.toString()).append("\n");
+
+            }
+        }
+        return mensaje.toString();
+    }
+
 
     /// SOBREESCRITURA
     @Override
@@ -95,8 +177,6 @@ public class Hotel{
                 ", usuarios=" + usuarios +
                 '}';
     }
-
-
 
 
 }
--- src/Modelo/Recepcionista.java
+++ src/Modelo/Recepcionista.java
@@ -6,9 +6,6 @@ public class Recepcionista extends Usuario {
 
 
 
-    ///ACÁ IRIAN TODAS LOS MÉTODOS DEL RECEPCIONISTA
-
-    //metodo  de la clase recepcionista : listar ocupadas con datos de ocupantes , habitaciones disponibles, y habitaciones NO disponibles por algún motivo
 
 
 
