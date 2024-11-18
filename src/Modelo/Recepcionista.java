9cb2e5459502cedc2fbf510469883717bbf04ebf
hewwwwwwwww <162480135+hewwwwwwwww@users.noreply.github.com>
2024-11-18 03:09:42 -0300

    Update Reserva/MenuRecepcionista
    Reserva:Metodo crear reserva
    
    MenuRecepcionista: Creación del menú (boceto)

--- src/Modelo/Recepcionista.java
+++ src/Modelo/Recepcionista.java
@@ -3,22 +3,24 @@ package Modelo;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Scanner;
+
 public class Recepcionista extends Usuario {
 
-    public Recepcionista(String nombre, String rol, String dni){
+    public Recepcionista(String nombre, String rol, String dni) {
         super(nombre, rol, dni);
     }
 
-    public  Recepcionista(){
+    public Recepcionista() {
 
     }
 
 
     /// JSON A RECEPCIONISTA
 
-    public static Usuario JSONArecepcionista(JSONObject jsonObject){
+    public static Usuario JSONArecepcionista(JSONObject jsonObject) {
         Recepcionista recepcionista = new Recepcionista();
-        try{
+        try {
             recepcionista.setId(jsonObject.getInt("id"));
             recepcionista.setNombre(jsonObject.getString("nombre"));
             recepcionista.setRol(jsonObject.getString("rol"));
@@ -32,22 +34,31 @@ public class Recepcionista extends Usuario {
 
 /// CHECK IN
 
-    public Reserva crearReserva(Cliente cliente){
+    public Reserva crearReserva(Cliente cliente, Habitacion habitacion) {
         Reserva nuevaReserva = new Reserva();
 
-        if(cliente == null){
+        if (cliente == null || habitacion == null) {
             throw new IllegalArgumentException("NULO");
         }
 
+        nuevaReserva.setCliente(cliente);
+        nuevaReserva.setHabitacion(habitacion);
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la fecha de inicio de la reserva.\n"); ///QUIZAS ESTE PRINT NO DEBERÍA IR ACÁ
+        String finicio = scanner.next();
+        if(!nuevaReserva.verificarFecha(finicio)){
+            throw new IllegalArgumentException();
+        }
+        System.out.println("Ingrese la fecha de fin de la reserva.\n");
+        String ffin = scanner.next();
 
 
 
 
         return nuevaReserva;
     }
-
-
 
 
 }
--- src/UI/MenuRecepcionista.java
+++ src/UI/MenuRecepcionista.java
@@ -8,44 +8,46 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Scanner;
+
 public class MenuRecepcionista {
 
 
     /// Constructor
-    public MenuRecepcionista(){
+    public MenuRecepcionista() {
 
     }
 
 
     /// Metodos
 
-    public void menu(){
+    public void menu() {
         /// LECTURA DE JSON
 
         Hotel hotel = new Hotel();
 
-        try{
+        try {
             JSONObject datosJSON = new JSONObject(JsonUtils.leer("hotel.json"));
 
             hotel.setNombre(datosJSON.getString("nombre"));
 
             JSONArray habitaciones = datosJSON.getJSONArray("habitaciones");
 
-            for(int i=0; i<habitaciones.length(); i++){
+            for (int i = 0; i < habitaciones.length(); i++) {
                 JSONObject jsonObject = habitaciones.getJSONObject(i);
                 hotel.agregar(Habitacion.JSONAhabitacion(jsonObject));
             }
 
             JSONArray usuarios = datosJSON.getJSONArray("usuarios");
 
-            for(int i=0; i<usuarios.length(); i++){
+            for (int i = 0; i < usuarios.length(); i++) {
                 JSONObject jsonObject = usuarios.getJSONObject(i);
                 hotel.agregar(Recepcionista.JSONArecepcionista(jsonObject));
             }
 
             JSONArray reservas = datosJSON.getJSONArray("reservas");
 
-            for(int i=0; i<reservas.length(); i++){
+            for (int i = 0; i < reservas.length(); i++) {
                 JSONObject jsonObject = reservas.getJSONObject(i);
                 hotel.agregar(Reserva.JSONAreserva(jsonObject));
             }
@@ -58,24 +60,85 @@ public class MenuRecepcionista {
         System.out.println(hotel);
 
 
-        System.out.println("Elegir una opcion: ");
-        System.out.println("1. CHECK IN");
-        System.out.println("2. CHECK OUT");
-        System.out.println("3. GUARDAR DATOS");
+        Scanner scanner = new Scanner(System.in);
+
+
+        int opcion;
+
+
+        do {
+
+
+            System.out.println("Elegir una opcion: \n");
+
+            System.out.println("1. CHECK IN");
+            System.out.println("2. CHECK OUT");
+            System.out.println("3. LISTAR HABITACIONES DISPONIBLES");
+            System.out.println("4. LISTAR HABITACIONES OCUPADAS");
+            System.out.println("5. LISTAR HABITACIONES NO DISPONIBLE");
+            System.out.println("6. CAMBIAR ESTADO HABITACION");
+            System.out.println("7. GUARDAR DATOS");
+            System.out.println("8. FINALIZAR EJECUCIÓN");
+
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+
+                case 1: {
 
 
 
 
+                    break;
+                }
+                case 2: {
 
+                    break;
+                }
+                case 3: {
+
+
+                    break;
+                }
+
+                case 4: {
+
+                    break;
+                }
+
+                case 5: {
+
+                    break;
+                }
+
+                case 6: {
+
+                    break;
+                }
+
+                case 7: {
+
+                    break;
+                }
+
+                case 8: {
+
+                    break;
+                }
+
+                default: {
+
+                    System.out.println("Finalizando ejecución");
+
+                    break;
+                }
+            }
+
+
+        } while (opcion != 8);
 
 
     }
-
-
-
-
-
-
 
 
 }
